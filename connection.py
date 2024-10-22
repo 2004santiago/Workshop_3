390dbd3c76ba17ad4a3e9240e1b7cf1614dbb619
santiago-04 <santiago.gomez.castro.04@gmial.com>
2024-10-22 16:05:09 -0500

    Commit saved

--- connection.py
+++ connection.py
@@ -11,17 +11,22 @@ nameDB = os.getenv('DB_NAME')
 userDB = os.getenv('DB_USER')
 passDB = os.getenv('DB_PASS')
 
-location_file = './Dirty-Data/the_grammy_awards.csv'  
-raw_table_database = 'grammy_awards'  
+location_file = './data/finalDataset.csv'  
+raw_table_database = 'score_country'  
+
+location_file2 = './data/modelDataset.csv'  
+raw_table_database2 = 'model_dataset'  
 
 engine = create_engine(f'postgresql+psycopg2://{userDB}:{passDB}@{localhost}:{port}/{nameDB}')
 
 try:
     df = pd.read_csv(location_file, sep=",")
-
     df.to_sql(raw_table_database, con=engine, if_exists='replace', index=False)
-
     print(f"Table '{raw_table_database}' created and data uploaded successfully.")
+    
+    df2 = pd.read_csv(location_file2, sep=",")
+    df2.to_sql(raw_table_database2, con=engine, if_exists='replace', index=False)
+    print(f"Table '{raw_table_database2}' created and data uploaded successfully.")
 
 except Exception as e:
     print(f"Error uploading data: {e}")
